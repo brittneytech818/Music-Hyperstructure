2e42076c0adef97475a79374debd48928d1edc1b
Brittney Spalding <brittney.tech.eng@gmail.com>
2022-08-09 21:26:50 -0300

    I break down monolith components into sub-components for more modular architecture.

---
+++ src/components/FormInput/FormInput.js
@@ -0,0 +1,22 @@
+import { Input } from 'degen'
+import { useFormContext } from 'react-hook-form'
+
+const FormInput = ({ id, label, min, placeholder, step, suffix, type }) => {
+	const { register } = useFormContext()
+
+	return (
+		<Input
+			width="180"
+			id={id}
+			label={label}
+			min={min}
+			placeholder={placeholder}
+			step={step}
+			suffix={suffix}
+			type={type}
+			{...register(id)}
+		/>
+	)
+}
+
+export default FormInput
